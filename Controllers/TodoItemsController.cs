6ed315fa39bdf8e128eabe54c835c4932aabbed8
L E N O V O <L E N O V O@LAPTOP-22HDPOTU>
2020-08-14 13:56:04 +0700

    New(TodoProject) : Create project Todo items

--- Controllers/TodoItemsController.cs
+++ Controllers/TodoItemsController.cs
@@ -9,7 +9,7 @@ using NetTestProject.Models;
 
 namespace NetTestProject.Controllers
 {
-    [Route("value/[controller]")]
+    [Route("api/[controller]")]
     [ApiController]
     public class TodoItemsController : ControllerBase
     {
@@ -20,16 +20,15 @@ namespace NetTestProject.Controllers
             _context = context;
         }
 
-        // GET: api/TodoItems
+        // GET: api/TodoItems : Get all Data in database
         [HttpGet]
         public async Task<ActionResult<IEnumerable<TodoItem>>> GetTodoItems()
         {
-            Console.WriteLine(_context.TodoItems.ToListAsync());
-            return new JsonResult(_context.TodoItems.ToListAsync());
+            return await _context.TodoItems.ToListAsync();
         }
 
-        // GET: api/TodoItems/5
-        [HttpGet("{id}")]
+        // GET: api/TodoItems/5 : Get spesific data by id unique (primary key)
+        [HttpGet("byId/{id}")]
         public async Task<ActionResult<TodoItem>> GetTodoItem(long id)
         {
             var todoItem = await _context.TodoItems.FindAsync(id);
@@ -41,6 +40,21 @@ namespace NetTestProject.Controllers
 
             return todoItem;
         }
+
+        // GET : api/TodoItems/byDate/2020-08-20/2020-08-21 : Get all data with spesific range date (start date and end date)
+        [HttpGet("byDate/{startDate}/{endDate}")]
+        public async Task<ActionResult<IEnumerable<TodoItem>>> GetTodoItemByDate(DateTime startDate, DateTime endDate)
+        {
+           return await _context.TodoItems.Where(p => p.FinishDate >= startDate && p.FinishDate <= endDate).ToListAsync();
+        }
+
+        // GET : api/TodoItems/byStatus/false : Get all data with spesific status (true or false)
+        [HttpGet("byStatus/{status}")]
+        public async Task<ActionResult<IEnumerable<TodoItem>>> GetTodoItemByStatus(bool status)
+        {
+            return await _context.TodoItems.Where(p => p.isComplate == status).ToListAsync();
+        }
+
 
         // PUT: api/TodoItems/5
         // To protect from overposting attacks, enable the specific properties you want to bind to, for
@@ -71,7 +85,38 @@ namespace NetTestProject.Controllers
                 }
             }
 
-            return NoContent();
+            return new JsonResult(todoItem);
+        }
+
+        // PATCH: api/TodoItems/setComplate/1/true : is used to set status complate (true or false) of spesific todo id
+        [HttpPatch("setComplate/{id}/{percent}")]
+        public async Task<ActionResult<TodoItem>> SetisComplate(bool percent, long id)
+        {
+            var todoItem = await _context.TodoItems.FindAsync(id);
+            if (todoItem == null)
+            {
+                return NotFound();
+            }
+
+            todoItem.isComplate = percent;
+            await _context.SaveChangesAsync();
+            return todoItem;
+        }
+
+
+        // PATCH: api/TodoItems/UpdatePercent/1/30 : is used to set percent value of spesific todo id
+        [HttpPatch("UpdatePercent/{id}/{percent}")]
+        public async Task<ActionResult<TodoItem>> SetPercentComplate(long percent, long id)
+        {
+            var todoItem = await _context.TodoItems.FindAsync(id);
+            if (todoItem == null)
+            {
+                return NotFound();
+            }
+
+            todoItem.percentComplate = percent;
+            await _context.SaveChangesAsync();
+            return todoItem;
         }
 
         // POST: api/TodoItems
@@ -83,9 +128,10 @@ namespace NetTestProject.Controllers
             _context.TodoItems.Add(todoItem);
             await _context.SaveChangesAsync();
 
-            //return CreatedAtAction("GetTodoItem", new { id = todoItem.Id }, todoItem);
-            return CreatedAtAction(nameof(GetTodoItem), new { id = todoItem.Id }, todoItem);
+            return CreatedAtAction("GetTodoItem", new { id = todoItem.Id }, todoItem);
         }
+
+
 
         // DELETE: api/TodoItems/5
         [HttpDelete("{id}")]
--- Models/TodoItem.cs
+++ Models/TodoItem.cs
@@ -7,8 +7,21 @@ namespace NetTestProject.Models
 {
     public class TodoItem
     {
-        public long Id { get; set; }
-        public string Name { get; set; }
-        public bool IsComplete { get; set; }
+        public long Id { get; set; } // unique id of each todo
+        public string Title { get; set; } // is used as title todo
+        public string Description { get; set; } // is used as description of each title
+        public DateTime DateCreated { get; set; }  // is Used as initial date created of  task todo
+        public DateTime? FinishDate { get; set; }   // is Used as Target of finisih task todo
+        public long percentComplate { get; set; }
+        public bool isComplate { get; set; }
+
+        // Set Default value
+        public TodoItem()
+        {
+            DateCreated = DateTime.Now;
+            FinishDate = DateTime.Now;
+            percentComplate = 0;
+            isComplate = false;
+        }
     }
 }
